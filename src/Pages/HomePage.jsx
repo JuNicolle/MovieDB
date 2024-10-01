cace875084adf53c3186f0093c25ea8b2ac34de7
JuNicolle <juliennicolle@hotmail.fr>
2024-10-01 14:59:06 +0200

    pagination fonctionnelle et changement de page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,11 +8,12 @@ import Pagination from 'react-bootstrap/Pagination';
 const HomePage = () => {
     const [movies, setMovies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [maxPage, setMaxPage] = useState(20);
+    const [maxPage, setMaxPage] = useState(500);
 
     const fetchMovies = async () => {
         try {
-            const response = await MoviesServices.getAllMovies();
+            const response = await MoviesServices.getAllMovies(currentPage);
+            // setMaxPage(response.data.total_pages);
             setMovies(response.data.results);
         } catch (error) {
             console.log(error);
@@ -21,42 +22,51 @@ const HomePage = () => {
 
     useEffect(() => {
         fetchMovies();
-    },[])
-        
-    
+    }, [currentPage])
+
+
     return <Container className="d-flex flex-column align-items-center">
-    <h1>Films du moment</h1>
-    <div className="d-flex justify-content-center flex-wrap gap-3">
-    {movies.map((movie) => {
-        return <MovieCard key={movie.id} movieCard={movie}></MovieCard>
-    })}
-    </div>
-    <Pagination className="mt-3">
-        {currentPage>1 && <>
-            <Pagination.First onClick={()=>{setCurrentPage(1)}} />
-      <Pagination.Prev onClick={()=>{setCurrentPage(currentPage-1)}}/>
-      <Pagination.Item onClick={()=>{setCurrentPage(1)}}>{1}</Pagination.Item>
-      <Pagination.Ellipsis onClick={()=>{setCurrentPage(currentPage-5)}} />
-
-      
-        </>}
-      
-      {(currentPage!=2&&currentPage>1)&& <>
-        <Pagination.Item onClick={()=>{setCurrentPage(currentPage-1)}}>{currentPage-1}</Pagination.Item>
-      </>}
-      <Pagination.Item onClick={()=>{setCurrentPage(currentPage)}} active>{currentPage}</Pagination.Item>
-        {currentPage<maxPage && <>
-
-            <Pagination.Item onClick={()=>{setCurrentPage(currentPage+1)}}>{currentPage+1}</Pagination.Item>
-
-            <Pagination.Ellipsis onClick={()=>{setCurrentPage(currentPage+5)}}/>
-            <Pagination.Item onClick={()=>{setCurrentPage(maxPage)}}>{maxPage}</Pagination.Item>
-            <Pagination.Next onClick={()=>{setCurrentPage(currentPage+1)}}></Pagination.Next>
-            <Pagination.Last onClick={()=>{setCurrentPage(maxPage)}}></Pagination.Last>
+        <h1>Films du moment</h1>
+        <div className="d-flex justify-content-center flex-wrap gap-3">
+            {movies.map((movie) => {
+                return <MovieCard key={movie.id} movieCard={movie}></MovieCard>
+            })}
+        </div>
+        <Pagination className="mt-5">
+            {currentPage > 1 && <>
+                <Pagination.First onClick={() => { setCurrentPage(1) }} />
+                <Pagination.Prev onClick={() => { setCurrentPage(currentPage - 1) }} />
+                <Pagination.Item onClick={() => { setCurrentPage(1) }}>{1}</Pagination.Item>
             </>}
-            </Pagination>
+
+            {currentPage - 5 > 0 && <>
+                <Pagination.Ellipsis onClick={() => { setCurrentPage(currentPage - 5) }} />
+            </>}
+
+
+            {(currentPage != 2 && currentPage > 1) && <>
+                <Pagination.Item onClick={() => { setCurrentPage(currentPage - 1) }}>{currentPage - 1}</Pagination.Item>
+            </>}
+
+            <Pagination.Item active>{currentPage}</Pagination.Item>
+
+            {currentPage + 1 < maxPage && <>
+                <Pagination.Item onClick={() => { setCurrentPage(currentPage + 1) }}>{currentPage + 1}</Pagination.Item>
+            </>}
+
+            {currentPage + 5 <= maxPage && <>
+                <Pagination.Ellipsis onClick={() => { setCurrentPage(currentPage + 5) }} />
+            </>}
+
+            {currentPage < maxPage && <>
+                <Pagination.Item onClick={() => { setCurrentPage(maxPage) }}>{maxPage}</Pagination.Item>
+                <Pagination.Next onClick={() => { setCurrentPage(currentPage + 1) }} />
+                <Pagination.Last onClick={() => { setCurrentPage(maxPage) }} />
+            </>}
+
+        </Pagination>
     </Container>
-    
+
 }
- 
+
 export default HomePage;
