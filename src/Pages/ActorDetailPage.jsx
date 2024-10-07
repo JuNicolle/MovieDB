eb2079827579928b32b188cfbabaa76221e918ca
JuNicolle <juliennicolle@hotmail.fr>
2024-10-07 10:46:17 +0200

    pagination actor page operationnal

--- src/Pages/ActorDetailPage.jsx
+++ src/Pages/ActorDetailPage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ActorsService from "../Services/ActorsService";
-import { Button } from "react-bootstrap";
 import MovieCard from "../Components/MovieCard";
+import { Pagination } from "react-bootstrap";
+
 
 const ActorDetailPage = () => {
     const { id } = useParams();
     const [actors, setActors] = useState({});
     const [actorsMovies, setActorsMovies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(20);
 
     const fetchActorById = async ()=>{
         try{
@@ -21,21 +24,23 @@ const ActorDetailPage = () => {
 
     const fetchMoviesByActorId = async ()=>{
         try {
-            const response = await ActorsService.getMoviesByActorId(id);
+            const response = await ActorsService.getMoviesByActorId(id, currentPage);
             console.log(response.data.results);
             setActorsMovies(response.data.results);
+            setMaxPage(response.data.total_pages);
             
         } catch (error) {
             console.log(error);  
         }
     }
 
-
-
-
 useEffect(()=>{
     fetchActorById(), fetchMoviesByActorId();
 },[])
+
+useEffect(()=>{
+    fetchMoviesByActorId();
+},[currentPage])
 
     return <> 
         <div className="d-flex flex-column align-items-center">
@@ -44,7 +49,7 @@ useEffect(()=>{
             <img style={{ height: '30rem' }} src={"https://image.tmdb.org/t/p/original"+actors.profile_path}></img>
             <div className="biography">
                 <p className="mt-4">
-                Biographie : <br />{actors.biography}
+                    Biographie : <br />{actors.biography}
                 </p> 
             </div>
         </div>
@@ -52,6 +57,40 @@ useEffect(()=>{
             {actorsMovies.map((actorsMovies) => {
                return <MovieCard movieCard={actorsMovies} key={actorsMovies.id} ></MovieCard>
             })}
+        </div>
+        <div className="d-flex flex-column align-items-center flex-wrap mt-2">
+        <Pagination>
+            {currentPage > 1 && <>
+                <Pagination.First onClick={() => { setCurrentPage(1) }} />
+                <Pagination.Prev onClick={() => { setCurrentPage(currentPage - 1) }} />
+                <Pagination.Item onClick={() => { setCurrentPage(1) }}>{1}</Pagination.Item>
+            </>}
+
+            {currentPage - 5 > 0 && <>
+                <Pagination.Ellipsis onClick={() => { setCurrentPage(currentPage - 5) }} />
+            </>}
+
+
+            {(currentPage != 2 && currentPage > 1) && <>
+                <Pagination.Item onClick={() => { setCurrentPage(currentPage - 1) }}>{currentPage - 1}</Pagination.Item>
+            </>}
+
+            <Pagination.Item active>{currentPage}</Pagination.Item>
+
+            {currentPage + 1 < maxPage && <>
+                <Pagination.Item onClick={() => { setCurrentPage(currentPage + 1) }}>{currentPage + 1}</Pagination.Item>
+            </>}
+
+            {currentPage + 5 <= maxPage && <>
+                <Pagination.Ellipsis onClick={() => { setCurrentPage(currentPage + 5) }} />
+            </>}
+
+            {currentPage < maxPage && <>
+                <Pagination.Item onClick={() => { setCurrentPage(maxPage) }}>{maxPage}</Pagination.Item>
+                <Pagination.Next onClick={() => { setCurrentPage(currentPage + 1) }} />
+                <Pagination.Last onClick={() => { setCurrentPage(maxPage) }} />
+            </>}
+        </Pagination>
         </div>
     </>
 }
